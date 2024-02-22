5af8e5197422701318b5c779ca542371c617ab62
ducica <dusanstojanovic256@gmail.com>
2024-02-22 07:20:15 +0100

    testing styled cmps

--- src/App.js
+++ src/App.js
@@ -4,150 +4,39 @@ import { BaseForm } from "react-invenio-forms";
 import { PermissionsField } from "./PermissionsField";
 import { Container, Dropdown, Grid } from "semantic-ui-react";
 import { useFormikContext } from "formik";
-import { FieldLabel } from "react-invenio-forms";
+import { FieldLabel, SelectField } from "react-invenio-forms";
 // import { ArrayField } from "./ArrayField";
+import { Formik } from "formik";
 import * as Yup from "yup";
 import { LocalVocabularySelectField } from "./LocalVocabularySelectField";
+import styled from "styled-components";
 
 const FormikStateLogger = () => {
   const state = useFormikContext();
   return <pre>{JSON.stringify(state, null, 2)}</pre>;
 };
-
-const loader = async (widget) => {
-  console.log(widget);
-  const module = await ((widget) => import(`./${widget}.js`))(widget);
-  console.log(module);
-  let component = module.default ?? module[widget];
-  console.log(component);
-  return React.createElement(component);
-};
-
-const initialValues = {
-  custom_fields: {
-    permissions: {
-      owner: [
-        "can_search",
-        "can_read",
-        "can_update",
-        "can_delete",
-        "can_create_files",
-        "can_set_content_files",
-        "can_get_content_files",
-        "can_commit_files",
-      ],
-      manager: [
-        "can_search",
-        "can_read",
-        "can_create",
-        "can_read_files",
-        "can_update_files",
-      ],
-      curator: [
-        "can_create",
-        "can_draft_set_content_files",
-        "can_search_drafts",
-        "can_draft_commit_files",
-      ],
-      reader: [
-        "can_get_content_files",
-        "can_read_files",
-        "can_search_drafts",
-        "can_search",
-        "can_read_draft",
-      ],
-    },
-  },
-};
-
-const uiData = {
-  permissions: [
-    {
-      group: "draft",
-      text: "Drafts",
-      permissions: [
-        { value: "can_edit", text: "Edit" },
-        { value: "can_new_version", text: "Create new draft version" },
-        { value: "can_search_drafts", text: "Search" },
-        { value: "can_read_draft", text: "Read" },
-        { value: "can_update_draft", text: "Update" },
-        { value: "can_delete_draft", text: "Delete" },
-        { value: "can_publish", text: "Publish" },
-        { value: "can_draft_create_files", text: "Create files" },
-        {
-          value: "can_draft_set_content_files",
-          text: "Set content for files",
-        },
-        {
-          value: "can_draft_get_content_files",
-          text: "Get content for files",
-        },
-        { value: "can_draft_commit_files", text: "Commit files" },
-        { value: "can_draft_read_files", text: "Read files" },
-        { value: "can_draft_update_files", text: "Update files" },
-      ],
-    },
-    {
-      group: "published_records",
-      text: "Records",
-      permissions: [
-        { value: "can_search", text: "Search" },
-        { value: "can_read", text: "Read" },
-        { value: "can_create", text: "Create" },
-        { value: "can_update", text: "Update" },
-        { value: "can_delete", text: "Delete" },
-        { value: "can_manage", text: "Manage" },
-      ],
-    },
-    {
-      group: "files",
-      text: "Files",
-      permissions: [
-        { value: "can_create_files", text: "Create" },
-        { value: "can_set_content_files", text: "Set content" },
-        { value: "can_get_content_files", text: "Get content" },
-        { value: "can_commit_files", text: "Commit" },
-        { value: "can_read_files", text: "Read" },
-        { value: "can_update_files", text: "Update" },
-        { value: "can_delete_files", text: "Delete" },
-      ],
-    },
-  ],
-  roles: [
-    { value: "owner", text: "Owner" },
-    { value: "manager", text: "Manager" },
-    { value: "curator", text: "Curator" },
-    { value: "reader", text: "Reader" },
-  ],
-};
-
+const dropdownOptions = [
+  { text: "Option 1", value: "Value 1" },
+  { text: "Option 2", value: "Value 2" },
+  { text: "Option 3", value: "Value 3" },
+  { text: "Option 4", value: "Value 4" },
+  { text: "Option 5", value: "Value 5" },
+  { text: "Option 6", value: "Value 6" },
+];
 function App() {
   return (
-    <BaseForm
-      formik={{
-        initialValues: initialValues,
-        // validationSchema: TestValidationSchema,
-        validateOnChange: false,
-        validateOnBlur: false,
-        enableReinitialize: true,
-      }}
-    >
-      <Container style={{ marginTop: "200px" }}>
-        <Grid>
-          <Grid.Column floated="left" width={9}>
-            <PermissionsField
-              label="Permissions"
-              labelIcon="user"
-              fieldPath="custom_fields.permissions"
-              uiData={uiData}
-            />
-          </Grid.Column>
-          <Grid.Column width={7}></Grid.Column>
-        </Grid>
-        <FormikStateLogger />
-      </Container>
-    </BaseForm>
+    <Formik>
+      <StyledDiv>
+        <SelectField options={dropdownOptions} placeholder="bla" />
+      </StyledDiv>
+    </Formik>
   );
 }
 
+const StyledDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
 export default App;
